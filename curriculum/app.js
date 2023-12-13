f751b878150ee73771f91a1c994f9ac9927c91f0
michaelF7 <guidellimichael@gmail.com>
2023-12-13 01:51:17 +0100

    Modifiche contenuto sezioni

--- curriculum/app.js
+++ curriculum/app.js
@@ -1,27 +1,24 @@
 function creaNumeri() {
+  let numeriDiv = document.getElementsByClassName("numeri-div")[0];
+  numeriDiv.innerHTML = "";
+  let altezzaNumeriInPx = 18;
+  let altezzaBody = document.body.offsetHeight;
 
-    let numeriDiv = document.getElementsByClassName("numeri-div")[0];
-    numeriDiv.innerHTML = "";
-    let altezzaNumeriInPx = 18;
-    let altezzaBody = document.body.offsetHeight;
+  for (i = 1; i < altezzaBody / altezzaNumeriInPx + 1; i++) {
+    let numero = document.createElement("p");
 
-    for (i = 1; i < (altezzaBody / altezzaNumeriInPx) + 1; i ++) {
-        
-        let numero = document.createElement("p");
-       
-        numero.className = "numeri-div";
+    numero.className = "numeri-div";
 
-        if (i < 10) {
-            numero.innerHTML = "0" + i;
-        } else {
-            numero.innerHTML = i;
-        }
-
-        numeriDiv.appendChild(numero);
+    if (i < 10) {
+      numero.innerHTML = "0" + i;
+    } else {
+      numero.innerHTML = i;
     }
 
+    numeriDiv.appendChild(numero);
+  }
 }
 
 window.onresize = function fire() {
-    creaNumeri();
+  creaNumeri();
 };
